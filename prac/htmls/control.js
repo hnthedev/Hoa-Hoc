6eb9d7273fa12b88cebcbd0c376ad1dae0c91e73
HN <nbminerga@gmail.com>
2023-12-11 18:26:22 +0700

    links

--- prac/htmls/control.js
+++ prac/htmls/control.js
@@ -3,17 +3,10 @@ var BTBtn = document.getElementsByClassName("TypeBtn2")[0];
 var MainFrame = document.getElementById("Fra");
 
 LyThuyetBtn.addEventListener('click', () => {
-    if (LyThuyetBtn.getAttribute("id") == "DIS") {
-        LyThuyetBtn.setAttribute("id","ACT");
-        BTBtn.setAttribute("id","DIS");
-        MainFrame.setAttribute('src',MainFrame.getAttribute('src').replace('BT','LYTHUYET'));
-    }
+    MainFrame.setAttribute('src',MainFrame.getAttribute('src').replace('BT','LYTHUYET'));
+    window.location = MainFrame.getAttribute("src");
 });
 BTBtn.addEventListener('click', () => {
-    if (BTBtn.getAttribute("id") == "DIS") {
-        BTBtn.setAttribute("id","ACT");
-        LyThuyetBtn.setAttribute("id","DIS");
-        MainFrame.setAttribute('src',MainFrame.getAttribute('src').replace('LYTHUYET','BT'));
-    };
-    
+    MainFrame.setAttribute('src',MainFrame.getAttribute('src').replace('LYTHUYET','BT'));
+    window.location = MainFrame.getAttribute("src");
 });
